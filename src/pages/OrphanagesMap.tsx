a3d6ea11c35b2125988c631559611cb4a73a0fe6
Stiverson <stiverson.machado@gmail.com>
2020-10-14 23:43:46 -0300

    Finalização dia 2 e 3 do nlw 3

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,49 +1,93 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {FiPlus} from 'react-icons/fi';
-import {Map, TileLayer} from 'react-leaflet';
+import React, { useEffect, useState } from 'react'
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import Leaflet from 'leaflet';
+import { FiArrowLeft } from 'react-icons/fi';
 
-import 'leaflet/dist/leaflet.css';
+import mapMarkerImg from '../images/map-marker.svg'
 
-import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css';
+import api from '../services/api';
 
-function OrphanagesMap(){
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58,68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+});
 
-    return(
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
-       <div id="page-map">
+function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
 
-           <aside>
-                <header>
+  useEffect(() => {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data);
+    });
+  }, []);
 
-                    <img src={mapMarkerImg} alt="Happy"/>
-                    <h2>Escolha um Orfanato no mapa.</h2>
-                    <p>Muitas crianças estão esperando a sua visita :)</p>
-                </header>
+  const { goBack } = useHistory();
 
-                <footer>
-                    <strong>Piracicaba</strong>
-                    <span>São Paulo</span>
-                </footer>
-           </aside>
-           
-           <Map 
-                center={[-22.7566274,-47.6159992]}
-                zoom={15}
-                style={{ width: '100%' , height: '100%' }}
-               >
-                   {/*<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}"/> */}
-               
-               <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
-               </Map>
-        
-           <Link to="/" className="create-orphanage">
-            <FiPlus size={32} color="#FFF" />
-           </Link>
-       </div>
-    );
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
 
+          <h2>Escolha um orfanato no mapa</h2>
+
+          <p>Muitas crianças estão esperando a sua visita {':)'}</p>
+        </header>
+
+        <footer>
+          <strong>Piracicaba</strong>
+          <span>São Paulo</span>
+          
+        </footer>
+      </aside>
+
+      <Map
+        center={[-22.7403848,-47.6373837]}
+        zoom={15}
+        style={{ width: '100%', height: '100%'}}
+      >
+       {/* <TileLayer
+         url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+       />*/}
+         <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        /> 
+
+        {orphanages.map(orphanage => (
+          <Marker
+            key={orphanage.id}
+            icon={mapIcon}
+            position={[orphanage.latitude, orphanage.longitude]}
+          >
+            <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+              {orphanage.name}
+              <Link to={`/orphanages/${orphanage.id}`}>
+                <FiArrowRight size={20} color="#fff" />
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
+      </Map>
+
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+      
+    </div>
+    
+  )
 }
 
 export default OrphanagesMap;
